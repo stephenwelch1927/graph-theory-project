4b01d200eec0f3bd906fc5ad92f7db12e1bf476d
Stephen Welch <stephenwelch1927@yahoo.com>
2021-02-01 18:39:33 +0000

    Looked at the basics

---
+++ basics.py
@@ -0,0 +1,14 @@
+# Stephen Welch
+# Python basics
+#2021-02-01
+
+print("Hello World!")
+
+a = 1
+b = 1.0
+s = "Hello, World from a string!"
+t = 'Hello, from a different string'
+
+print(a, b, s, t)
+
+print(s[3:10:2])
